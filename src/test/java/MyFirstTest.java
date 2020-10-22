f365e677bd9d9f63361f8a2fc674cc5ec1fd6c92
Chelsea <chelsea.e.rolland@gmail.com>
2020-10-21 19:03:27 -0500

    completed the review exercise

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -2,6 +2,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -22,13 +23,15 @@ public class MyFirstTest {
     @Test
     public void testIfCompanyNameIsEquals(){
         String expected = "Codeup";
-        String actual = "CodeUp";
+        String actual = "Codeup";
+        String badOne = "CodeUp";
         //Manual Method
         //expected.equalsIgnoreCase(actual);
 
         //JUnit Method --> Assert
-        //assertEquals(expected, actual);
-        assertNotEquals(expected, actual);
+        assertEquals(expected, actual);
+        assertNotEquals(expected, actual.toLowerCase());
+        assertNotEquals(expected, badOne);
     }
 
     @Test
@@ -93,11 +96,14 @@ public class MyFirstTest {
     public void testIfArraysAreSame(){
         int[] numbers = {1, 2, 3};
         int[] otherNumbers = new int[3];
+        int[] moreNumbers = {3, 2, 1};
         otherNumbers[0] = 1;
         otherNumbers[1] = 2;
         otherNumbers[2] = 3;
 
         assertArrayEquals(numbers, otherNumbers);
+        //Utilized below since there is no method of assertArrayNotEqual
+        assertFalse(Arrays.equals(numbers, moreNumbers));
     }
 
     @Test
